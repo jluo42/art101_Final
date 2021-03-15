d1d6c38fe7bf3b1c82e83b2f38651a4bfded9661
jluo42 <jluo42@ucsc.edu>
2021-03-15 14:27:29 -0700

    updating project

--- js/main.js
+++ js/main.js
@@ -31,3 +31,27 @@ function topFunction() {
 function changeBrowser() {
   window.open("https://www.gardeningknowhow.com/ornamental/cacti-succulents/burros-tail/burros-tail-care.htm", "_blank");
 }
+
+var opacity = 0;
+var intervalID = 0;
+
+function changeImage() {
+  console.log("I am being clicked");
+  intervalID = setInterval(hide, 20);
+}
+
+function hide () {
+  var img = document.getElementById("plant2");
+  opacity = Number(window.getComputedStyle(img).getPropertyValue("opacity"));
+
+  if(opacity > 0) {
+    opacity = opacity - 0.1;
+    img.style.opacity = opacity;
+  } else {
+    clearInterval(intervalID);
+  }
+}
+
+function showInfograph() {
+
+}
